0b699b9e958fd4ec8fed475ef259f53d6793787b
Andrew Seba <sebaturtle@gmail.com>
2017-04-05 17:13:24 -0500

    Started zml importing

---
+++ MegaKittenTrail/Assets/Scripts/Common/TileLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Xml;
+using UnityEngine;
+
+/// <summary>
+/// Author: Andrew Seba
+/// Description: Loads the background tiles from the XML
+/// </summary>
+public class TileLoader : MonoBehaviour
+{
+    //Holds the .xml
+    public TextAsset mapInformation;
+    //Temp tile obj.
+    public GameObject tempCube;
+    private Sprite[] sprites;
+
+    // Use this for initialization
+    void Start()
+    {
+        sprites = Resources.LoadAll<Sprite>("MegaKittenTrailSpriteSheet copy");
+        XmlDocument xmlDoc = new XmlDocument();
+    }
+}
