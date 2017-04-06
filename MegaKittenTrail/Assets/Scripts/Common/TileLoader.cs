d654bf95aa63a6e58e5ad734048f2d751617cb0a
Andrew Seba <sebaturtle@gmail.com>
2017-04-06 18:49:50 -0500

    Tile loader complete not tested

--- MegaKittenTrail/Assets/Scripts/Common/TileLoader.cs
+++ MegaKittenTrail/Assets/Scripts/Common/TileLoader.cs
@@ -10,6 +10,8 @@ public class TileLoader : MonoBehaviour
 {
     //Holds the .xml
     public TextAsset mapInformation;
+    public int layerWidth;
+    public int layerHeight;
     //Temp tile obj.
     public GameObject tempCube;
     private Sprite[] sprites;
@@ -17,7 +19,68 @@ public class TileLoader : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        StartCoroutine(LoadMap());
+    }
+
+    IEnumerator LoadMap()
+    {
+        yield return new WaitForEndOfFrame();
         sprites = Resources.LoadAll<Sprite>("MegaKittenTrailSpriteSheet copy");
         XmlDocument xmlDoc = new XmlDocument();
+
+        xmlDoc.LoadXml(mapInformation.text);
+
+        XmlNodeList layerNames = xmlDoc.GetElementsByTagName("layer");
+
+        XmlNode tilesetInfo = xmlDoc.SelectSingleNode("map").SelectSingleNode("tileset");
+        float tilewidth = float.Parse(tilesetInfo.Attributes["tilewidth"].Value) / 16f;
+        float tileHeight = float.Parse(tilesetInfo.Attributes["tileheight"].Value) / 16f;
+
+        foreach (XmlNode layerInfo in layerNames)
+        {
+            layerWidth = int.Parse(layerInfo.Attributes["width"].Value);
+            layerHeight = int.Parse(layerInfo.Attributes["height"].Value);
+
+            XmlNode tempNode = layerInfo.SelectSingleNode("data");
+
+            int verticalIndex = layerHeight - 1;
+            int horizontalIndex = 0;
+
+            foreach (XmlNode tile in tempNode.SelectNodes("tile"))
+            {
+                int spriteValue = int.Parse(tile.Attributes["gid"].Value);
+
+                //if sprite is not empty
+                if (spriteValue > 0)
+                {
+                    Sprite[] currentSpriteSheet = sprites;
+
+                    GameObject tempSprite = new GameObject(layerInfo.Attributes["name"].Value + " <" + horizontalIndex + ", " + verticalIndex + ">");
+
+                    //Add the tile script here if needed
+
+                    SpriteRenderer spriteRen = tempSprite.AddComponent<SpriteRenderer>();
+                    //get sprite from sheet
+                    spriteRen.sprite = currentSpriteSheet[spriteValue - 1];
+                    //set position
+                    tempSprite.transform.position = new Vector3((tilewidth * horizontalIndex), (tileHeight * verticalIndex));
+                    //set the sorting layer
+                    spriteRen.sortingLayerName = layerInfo.Attributes["name"].Value;
+
+                    //set parent
+                    tempSprite.transform.parent = GameObject.Find(layerInfo.Attributes["name"].Value + "Layer").transform;
+                    tempSprite.tag = "Tile";
+
+
+                }
+                horizontalIndex++;
+                if(horizontalIndex % layerWidth == 0)
+                {
+                    verticalIndex--;
+                    horizontalIndex = 0;
+                }
+            }
+        }
+
     }
 }
