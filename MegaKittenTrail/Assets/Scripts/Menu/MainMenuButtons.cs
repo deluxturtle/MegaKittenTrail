d4f95b23c9ef3bcc6c71a5191be34a331ef9c68c
Andrew Seba <sebaturtle@gmail.com>
2017-05-11 15:16:45 -0500

    Next to attach script to button

---
+++ MegaKittenTrail/Assets/Scripts/Menu/MainMenuButtons.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Author: Andrew Seba
+/// Description: Handles main menu buttons
+/// </summary>
+public class MainMenuButtons : MonoBehaviour {
+
+    /// <summary>
+    /// Temporary start game to load the main gameplay scene.
+    /// </summary>
+	public void _StartGame()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
