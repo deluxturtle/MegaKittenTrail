82dfda1749387d706edfdec8363c19033d0bfa88
Andrew Seba <sebaturtle@gmail.com>
2017-06-13 09:44:55 -0500

    Updated to newest version

---
+++ MegaKittenTrail/Assets/Scripts/EventDialog.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Author: Andrew Seba
+/// Description:
+/// A script to find these references easily that belongs on the event dialog object.
+/// </summary>
+public class EventDialog : MonoBehaviour {
+
+    public Text textBig;
+    public Button buttonOk;
+}
